f3675923ccd013fb893244cfe5857dbfb654af6e
Al Maz <alexmaznitskiy@gmail.com>
2023-12-13 16:30:37 +0300

    fixes LR5

--- LR5/MotionTracking.py
+++ LR5/MotionTracking.py
@@ -1,18 +1,24 @@
 import cv2
 import numpy as np
+
 i = 0
+
 # Здесь определяется функция main с четырьмя аргументами:
 # kernel_size (размер ядра для гауссова размытия),
 # standard_deviation (стандартное отклонение для гауссова размытия),
 # delta_tresh (порог значений для бинаризации разности между кадрами)
 # и min_area (минимальная площадь контура для рассмотрения).
+
 def main(kernel_size, standard_deviation, delta_tresh, min_area):
     global i
     i += 1
-    video = cv2.VideoCapture('vid/main_video.mov', cv2.CAP_ANY)
+
+    video = cv2.VideoCapture('videos/main_video.mov', cv2.CAP_ANY)
+
     ret, frame = video.read()
     img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     img = cv2.GaussianBlur(img, (kernel_size, kernel_size), standard_deviation)
+
     w = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
     h = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
     fourcc = cv2.VideoWriter_fourcc(*'mp4v')
@@ -23,6 +29,7 @@ def main(kernel_size, standard_deviation, delta_tresh, min_area):
     # вышеуказанный кодек для записи выводимого видео с заданной
     # частотой кадров(144 кадров в секунду) и размерами кадров w на h.
 
+
     while True:
         # сохраняем старый кадр чтобы вычислить разниц между кадрами
         print("...")
@@ -30,6 +37,7 @@ def main(kernel_size, standard_deviation, delta_tresh, min_area):
         ok, frame = video.read()
         if not ok:
             break
+
         img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         img = cv2.GaussianBlur(img, (kernel_size, kernel_size), standard_deviation)
 
@@ -44,3 +52,40 @@ def main(kernel_size, standard_deviation, delta_tresh, min_area):
         # cv2.imshow("a",thresh)
         # находим контуры
         (contors, hierarchy) = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+
+        # если на кадре есть хотя бы один контур, чья площадь достаточно большая то записываем кадр
+        for contr in contors:
+            area = cv2.contourArea(contr)
+            if area < min_area:
+                continue
+            video_writer.write(frame)
+
+    video_writer.release()
+
+    print("готово!")
+
+kernel_size = 3
+standard_deviation = 50
+delta_tresh = 60
+min_area = 20
+main(kernel_size, standard_deviation, delta_tresh, min_area)
+
+# оптимальный вариант
+kernel_size = 11
+standard_deviation = 70
+delta_tresh = 60
+min_area = 20
+main(kernel_size, standard_deviation, delta_tresh, min_area)
+
+kernel_size = 3
+standard_deviation = 50
+delta_tresh = 20
+min_area = 20
+main(kernel_size, standard_deviation, delta_tresh, min_area)
+
+kernel_size = 3
+standard_deviation = 50
+delta_tresh = 60
+min_area = 10
+main(kernel_size, standard_deviation, delta_tresh, min_area)
